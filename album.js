34479b1eb894ec33231923e6a98f28aa8d2e3b7b
231130518 <caprian.revaldo@students.mikroskil.ac.id>
2024-10-30 19:46:47 +0700

    Updates

---
+++ album.js
@@ -0,0 +1,364 @@
+export const album = [
+  {
+    id: 76987687,
+    judul: "The 5th Mini Album [Whiplash] (Whiplash Limited Ver.) (Random)",
+    stok: 982,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/f3895ec94202d41a6dbf849fcdda1858.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/ef6e0bd2c596034a689597bec9bcdfb8.png?q=95&w=720",
+    kategori: "album",
+    harga: 716257,
+    status: "EXCLUSIVE",
+    artist: "aespa",
+  },
+  {
+    id: 45677,
+    judul: "The 1st Album [Armageddon] (Authentic Ver.) (Random)",
+    stok: 0,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/44ff254b113e4f0ec691433363a1e11a.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/1d1a7b9818c6913c9b874a7698891a7c.jpg?q=95&w=720",
+    kategori: "album",
+    harga: 370007,
+    status: "SOLD OUT",
+    artist: "aespa",
+  },
+  {
+    id: 456464,
+    judul: "1st FULL ALBUM [DRIP] YG TAG ALBUM Ver. Set",
+    stok: 982,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/7121c91c749b1dda9da97900fc4d5d62.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/5907472f731f40e57165ec3ba8efa767.png?q=95&w=720",
+    kategori: "album",
+    harga: 1125660.8,
+    status: "PRE-ORDER",
+    artist: "BABYMONSTER",
+  },
+  {
+    id: 3466663,
+    judul: "BLACKPINK 2nd ALBUM [BORN PINK] DIGIPACK ver. SET",
+    stok: 0,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/3d5b2b5c4ce57aa772de44debbe5e12a.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/595ff7aa309cadc08438753087dcc337.png?q=95&w=720",
+    kategori: "album",
+    harga: 673808.85,
+    status: "SOLD OUT",
+    artist: "BLACKPINK",
+  },
+  {
+    id: 34546,
+    judul: "YEONJUN's Mixtape: GGUM",
+    stok: 0,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/ddf7e0b914933062d64b0f72a5deb39f.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/ef6e0bd2c596034a689597bec9bcdfb8.png?q=95&w=720",
+    kategori: "merchandise",
+    harga: 382872,
+    status: "SOLD OUT",
+    artist: "TOMORROW X TOGETHER",
+  },
+  {
+    id: 57854,
+    judul: "[LUCKY DRAW] ILLIT 2nd Mini Album 'I'LL LIKE YOU' (Set)",
+    stok: 893,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/77ea97dcfb028b4be6597a73c29461de.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/3e1b6852169568292994e881a82202f7.png?q=95&w=720",
+    kategori: "album",
+    harga: 738395.85,
+    status: "PRE-ORDER",
+    artist: "ILLIT",
+  },
+  {
+    id: 489695,
+    judul:
+      "1st Mini Album ['SUPER REAL ME' (Set) + 'SUPER REAL ME' (Weverse album ver.)]",
+    stok: 982,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/d7bd64619246da7dfd118a0f7855fa49.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/6a9a90060d74e9446281e8dc8e1032f8.png?q=95&w=720",
+    kategori: "album",
+    harga: 595445.4,
+    status: "READY",
+    artist: "ILLIT",
+  },
+  {
+    id: 5447568,
+    judul: "2nd Mini Album 'I'LL LIKE YOU' (GLLIT ver.) (Set)",
+    stok: 3478,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/4a447dd32bdb2a1074bc9aecace37274.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/595ff7aa309cadc08438753087dcc337.png?q=95&w=720",
+    kategori: "album",
+    harga: 566202,
+    status: "READY",
+    artist: "ILLIT",
+  },
+  {
+    id: 348768347,
+    judul: "S/S T-Shirt",
+    stok: 1489,
+    deskripsi: "EXCLUSIVE MERCHANDISE",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/fffbfb9bc455b70546bfa55f55dafbe7.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/be4d205f9ae5596c3767e5b0fc7c8f80.png?q=95&w=720",
+    kategori: "merchandise",
+    harga: 573979.5,
+    status: "READY",
+    artist: "ILLIT",
+  },
+  {
+    id: 1435460,
+    judul: "BABYMONSTER 1st MINI ALBUM [BABYMONS7ER] YG TAG ALBUM VER. SET",
+    stok: 0,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/4d1ba46eeafd478e16f654796e5a0143.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/8a085160712f34f5a27fb9aabbc19028.png?q=95&w=720",
+    kategori: "album",
+    harga: 937344.3,
+    status: "SOLD OUT",
+    artist: "BABYMONSTER",
+  },
+  {
+    id: 1436541,
+    judul: "BABYMONSTER 1st MINI ALBUM [BABYMONS7ER] PHOTOBOOK VER.",
+    stok: 32,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/0d1cdb3c9dad94ff141f98d88a24056b.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/3e92b9ab8d9595b5b9936f033a9fc2bc.png?q=95&w=720",
+    kategori: "album",
+    harga: 226947.45,
+    status: "READY",
+    artist: "BABYMONSTER",
+  },
+  {
+    id: 1342,
+    judul: "BABYMONSTER 1st MINI ALBUM [BABYMONS7ER] YG TAG ALBUM VER. OPTION",
+    stok: 340,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/f34522f8951cce3acaf8feecb34d74d9.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/c0a72539f8cf9a7c40947199d21ee6d4.png?q=95&w=720",
+    kategori: "album",
+    harga: 133928.55,
+    status: "READY",
+    artist: "BABYMONSTER",
+  },
+  {
+    id: 495793,
+    judul: "OFFICIAL LIGHT STICK ver.2",
+    stok: 340,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/c0bb0cade706c6aa6e1f830d047924a0.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/a222b08807c749e10f82442b09250883.png?q=95&w=720",
+    kategori: "album",
+    harga: 497448.9,
+    status: "EXCLUSIVE",
+    artist: "BLACKPINK",
+  },
+  {
+    id: 984987,
+    judul: "[THEALBUM] ROBE & SLEEPING MASK SET (BLACK)",
+    stok: 340,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/b3cc3a1428bae2a4a7b07122e4280876.jpg?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/a222b08807c749e10f82442b09250883.png?q=95&w=720",
+    kategori: "merchandise",
+    harga: 1721938.5,
+    status: "EXCLUSIVE",
+    artist: "BLACKPINK",
+  },
+  {
+    id: 348759,
+    judul: "[LALISA] TEAM JACKET",
+    stok: 340,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/51af82874f571a76a873970b937bf1b6.jpg?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/d7a0b0b67d3c7a01dc652b1804abae6d.jpg?q=95&w=720",
+    kategori: "merchandise",
+    harga: 2538264.9,
+    status: "EXCLUSIVE",
+    artist: "BLACKPINK",
+  },
+  {
+    id: 348576,
+    judul: "The 5th Mini Album [Whiplash] (SPEED Ver.) (Random)",
+    stok: 340,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/4b1b36ddf4bde23f5387831bf99395b4.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/e705c4f277abbc1cdeb90b93814dd24e.png?q=95&w=720",
+    kategori: "album",
+    harga: 246080.1,
+    status: "READY",
+    artist: "aespa",
+  },
+  {
+    id: 238479,
+    judul: "aespa 1st Concert SYNK : HYPER LINE CONCERT PHOTOBOOK",
+    stok: 0,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/ac335688858a0ec32a5603c8515f6308.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/cd0d2570977fb2c44a152f2ec4d2add1.png?q=95&w=720",
+    kategori: "album",
+    harga: 382497.45,
+    status: "SOLD OUT",
+    artist: "aespa",
+  },
+  {
+    id: 7958375,
+    judul: "‘SIS (Soft Is Strong)’ Set",
+    stok: 238,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/f0a29cf49f03bd42d8629e738e3bad68.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/35b52d162bfb97df7889f02489c11bff.png?q=95&w=720",
+    kategori: "album",
+    harga: 454050.45,
+    status: "READY",
+    artist: "KATSEYE",
+  },
+  {
+    id: 3576875,
+    judul: "Photocard + Tin case set",
+    stok: 238,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/0c7701efc0b56a1806cfa229cf37b2c5.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/c9c43174dfefde9306b198a4ca40565b.png?q=95&w=720",
+    kategori: "album",
+    harga: 277967.85,
+    status: "EXCLUSIVE",
+    artist: "KATSEYE",
+  },
+  {
+    id: 46577,
+    judul: "Treasure Official Light Stick",
+    stok: 238,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/c7ec207d5ecd96285a8f1b49262964cc.jpg?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/01080a0b7f82c4f2f335dde7161138c8.jpg?q=95&w=720",
+    kategori: "album",
+    harga: 4979448.9,
+    status: "EXCLUSIVE",
+    artist: "TREASURE",
+  },
+  {
+    id: 92993884,
+    judul: "[FUNNYTREASURES2] TREASURE TIN CASE STICKER PACK",
+    stok: 238,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/1be7d46e8213d209698bc817274d09bd.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/893cd204a6e2aeac1407c6843ed8bc28.jpg?q=95&w=720",
+    kategori: "album",
+    harga: 191170.9,
+    status: "EXCLUSIVE",
+    artist: "TREASURE",
+  },
+  {
+    id: 492874,
+    judul: "JAPAN 1st SINGLE [Here I Stand] (CD + DVD + 스마프라 무비&뮤직)",
+    stok: 238,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/a1541925d62836f5eb8e2949ab073e84.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/7e681e086a4a8940387dfb11a4ee8d4e.png?q=95&w=720",
+    kategori: "album",
+    harga: 1382528.4,
+    status: "READY",
+    artist: "TREASURE",
+  },
+  {
+    id: 29837384,
+    judul: "2nd MINI ALBUM [THE SECOND STEP : CHAPTER TWO] DIGIPACK ver. (SET)",
+    stok: 238,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/34251e5e8587e44bc6aba944c7cf9aa1.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/ab453d0cfed703556c738c7464378be7.png?q=95&w=720",
+    kategori: "album",
+    harga: 1390305.9,
+    status: "READY",
+    artist: "TREASURE",
+  },
+  {
+    id: 9287463,
+    judul: "4th Mini Album 'CRAZY' (Set)",
+    stok: 238,
+    deskripsi:
+      "100% of the album sales on Weverse Shopwill count for Hanteo Chart and Circle Chart",
+    gambar:
+      "https://cdn-contents.weverseshop.io/public/shop/aebfa2baea57ceb5717bc474eb56fca5.png?q=95&w=720",
+    gambardesk:
+      "https://cdn-contents.weverseshop.io/public/shop/7c108869741fcae42e24fd0e2e11cfc8.png?q=95&w=720",
+    kategori: "album",
+    harga: 746906.55,
+    status: "READY",
+    artist: "LESSERAFIM",
+  },
+];
+
+export default album;
